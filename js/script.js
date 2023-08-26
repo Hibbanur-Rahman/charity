c05136425478e8ac0eaf8492670135cda3e497ba
HIBBANUR RAHMAN <117516111+Hibbanur-Rahman@users.noreply.github.com>
2023-08-27 04:03:11 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -128,3 +128,21 @@ radio.forEach((element)=>{
         }
     })
 })
+
+
+
+//responsive ness
+var hamburger=document.querySelector('.hamburger');
+var navRight=document.querySelector('#nav-right');
+var navCount=0;
+hamburger.addEventListener('click',()=>{
+    if(navCount%2==0){
+        navRight.style.top='8vh';
+        navRight.style.transition=' all 0.5s ease-in-out';
+    }
+    else{
+        navRight.style.top='-40vh';
+    }
+    navCount++;
+   
+})
